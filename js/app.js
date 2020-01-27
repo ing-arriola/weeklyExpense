3d413f29ec748051946804a03dd752723d11bb3d
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-26 19:13:06 -0600

    Added error msg form incomplete form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const userExpense=prompt('Insert your weekly expense: ')//Getting the expense from the user
+const form=document.getElementById('agregar-gasto')
 
 class Expense{
     constructor(expense){
@@ -19,11 +20,35 @@ class Interfaz{
         expenseSpan.innerHTML=`${amount}`
         restOfExpense.innerHTML=`${amount}`
     }
+    printMessage(message,typeOfMessage){
+        const msgCnt=document.createElement('div')
+        msgCnt.classList.add('text-center','alert')
+        
+        
+        if (typeOfMessage==='error') {
+            msgCnt.classList.add('alert-danger')
+        } else {
+            
+        }
+        //Add the message in the div
+        msgCnt.appendChild(document.createTextNode(message))
+        //Add the div with the message before the form
+        document.getElementById('colWithForm').insertBefore(msgCnt,form)
+
+        setTimeout(function(){
+            document.querySelector('.primario .alert').remove()
+        },3000)
+
+    }
 }
 
-
+//Event Listerners
 document.addEventListener('DOMContentLoaded',askData)
 
+form.addEventListener('submit',addPurchase)
+
+
+//Definition of functions
 function askData(){
     if (userExpense === null || userExpense === '') {
         window.location.reload()
@@ -32,5 +57,19 @@ function askData(){
         //Getting a interfaz instance to send the data to the DOM
         interfaz=new Interfaz()
         interfaz.insertExpense(expense.expense)
+    }
+}
+
+function addPurchase(e){
+    e.preventDefault()
+    const nameOfPurchase=document.getElementById('gasto').value
+    const amountOfPurchase=document.getElementById('cantidad').value
+
+    const ui=new Interfaz()
+
+    if (nameOfPurchase === '' || amountOfPurchase === '') {
+        ui.printMessage('There was an error','error')
+    } else {
+        console.log('purchase Added')
     }
 }
