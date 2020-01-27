8f5b308bf5ed6c0d483648f34e5001aa542d7def
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-26 18:25:12 -0600

    Added interfaz to send data to DOM

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
-const userExpense=prompt('Insert your weekly expense: ')
+const userExpense=prompt('Insert your weekly expense: ')//Getting the expense from the user
 
-class expense{
+class Expense{
     constructor(expense){
     this.expense=Number(expense)  
     this.rest=Number(expense)
     }
     restOfExpense(amount=0){
-        return this.rest-=amount
+        return this.rest-=amount//
+    }
+}
+
+class Interfaz{
+    insertExpense(amount){
+        console.log(amount)
+        const expenseSpan=document.querySelector('span#total')
+        const restOfExpense=document.querySelector('span#restante')
+
+        expenseSpan.innerHTML=`${amount}`
+        restOfExpense.innerHTML=`${amount}`
     }
 }
 
@@ -17,6 +28,9 @@ function askData(){
     if (userExpense === null || userExpense === '') {
         window.location.reload()
     } else {
-        expense= new expense(userExpense)
+        expense= new Expense(userExpense)
+        //Getting a interfaz instance to send the data to the DOM
+        interfaz=new Interfaz()
+        interfaz.insertExpense(expense.expense)
     }
 }
